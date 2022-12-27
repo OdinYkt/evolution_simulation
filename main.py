3cdd130d4b74446a4359f290113d02b3f7054274
OdinYkt <kaktusaprel@gmail.com>
2022-12-27 15:47:00 +0300

    add create_food() repaired crt_live() other..

--- main.py
+++ main.py
@@ -9,103 +9,152 @@ def crt_cell(): #создание сетки отображения
         for j in range(size):
             cells[i][j].place(x=indent+i*21, y=indent+j*22)
 
-def crt_live(count): #создание организмов
+
+def crt_live(count):                                                    #создание организмов
     global live, c_live, n_live, n
     n = count
     live = [[randrange(0, 64) for i in range(64)] for j in range(n)]
-    for j in range(n):              #добавление хп в 65е значение в списке генома
+
+    for j in range(n):                                                  #добавление хп в 65е значение в списке генома
         live[j].append(10)
 
-    c_live = [[randrange(0, 32) for i in range(2)] for j in range(n)] #возможно два бота на одной позиции
-    n_live = [i for i in range(n)]
-    for i in range(n):
+    c_live = []                                                         #координаты живых клеток
+    j = 0
+    while j<n:
+            xy = [randrange(0, 32), randrange(0, 32)]
+            if not(xy in c_live):
+                c_live.append(xy)
+                j += 1
+
+    n_live = [i for i in range(n)]                                      #сбор количества живых клеток
+
+    for i in range(n):                                                  #покрас живых клеток
         x, y = c_live[i][0], c_live[i][1]
         cells[x][y].configure(background='red')
 
 
-def move(side,n):       #стороны перепутаны, но задействованы все
-    x, y = c_live[n][0], c_live[n][1]
+def create_food(count_of_food):
+    global food_coord
+    food_coord = []
+    i = 0
+    while i<count_of_food:
+        xy = [randrange(0, 32), randrange(0, 32)]
+        if not(xy in c_live) and not(xy in food_coord):
+            food_coord.append(xy)
+            i += 1
+            cells[xy[0]][xy[1]].configure(background='green')
+
+
+def move(side,index_of_bot):       #стороны перепутаны, но задействованы все
+    c = index_of_bot
+    x, y = c_live[c][0], c_live[c][1]
     if side == 0 and not([x - 1, y - 1] in c_live):
         cells[x][y].configure(background='#9c9192')
         x += -1
         y += -1
-        c_live[n] = [x, y]
+        c_live[c] = [x, y]
         cells[x][y].configure(background='red')
     elif side == 1 and not([x - 1, y + 1] in c_live):
         cells[x][y].configure(background='#9c9192')
         x += -1
         y += 1
-        c_live[n] = [x, y]
+        c_live[c] = [x, y]
         cells[x][y].configure(background='red')
     elif side == 2 and not ([x - 1, y] in c_live):
         cells[x][y].configure(background='#9c9192')
         x += -1
-        y += 0
-        c_live[n] = [x, y]
+        c_live[c] = [x, y]
         cells[x][y].configure(background='red')
     elif side == 3 and not ([x, y - 1] in c_live):
         cells[x][y].configure(background='#9c9192')
-        x += 0
         y += -1
-        c_live[n] = [x, y]
+        c_live[c] = [x, y]
         cells[x][y].configure(background='red')
     elif side == 4 and not ([x, y + 1] in c_live): #
         cells[x][y].configure(background='#9c9192')
-        x += 0
         y += 1
-        c_live[n] = [x, y]
+        c_live[c] = [x, y]
         cells[x][y].configure(background='red')
     elif side == 5 and not ([x + 1, y - 1] in c_live):
         cells[x][y].configure(background='#9c9192')
         x += 1
         y += -1
-        c_live[n] = [x, y]
+        c_live[c] = [x, y]
         cells[x][y].configure(background='red')
     elif side == 6 and not ([x, y + 1] in c_live): #
         cells[x][y].configure(background='#9c9192')
         x += 1
         y += 1
-        c_live[n] = [x, y]
+        c_live[c] = [x, y]
         cells[x][y].configure(background='red')
     elif side == 7 and not ([x + 1, y] in c_live):
         cells[x][y].configure(background='#9c9192')
         x += 1
-        y += 0
-        c_live[n] = [x, y]
+        c_live[c] = [x, y]
         cells[x][y].configure(background='red')
 
-def eat(side, n):
-    x, y = c_live[n][0], c_live[n][1]
+
+def eat(side, index_of_bot):
+    x, y = c_live[index_of_bot][0], c_live[index_of_bot][1]
     flag = False
-    if side == 0 and [x - 1, y - 1] in c_live:
-        c_victim = c_live.index([x - 1, y - 1])
+    if side == 8 and [x - 1, y - 1] in c_live:
+        index_of_victim = c_live.index([x - 1, y - 1])
         flag = True
-    elif side == 1 and not ([x - 1, y + 1] in c_live):
-        c_victim = c_live.index([x - 1, y + 1])
+    elif side == 9 and [x - 1, y + 1] in c_live:
+        index_of_victim = c_live.index([x - 1, y + 1])
         flag = True
-    elif side == 2 and not ([x - 1, y] in c_live):
-        c_victim = c_live.index([x - 1, y=])
+    elif side == 10 and [x - 1, y] in c_live:
+        index_of_victim = c_live.index([x - 1, y])
         flag = True
-    elif side == 3 and not ([x, y - 1] in c_live):
-        c_victim = c_live.index([x, y - 1])
+    elif side == 11 and [x, y - 1] in c_live:
+        index_of_victim = c_live.index([x, y - 1])
         flag = True
-    elif side == 4 and not ([x, y + 1] in c_live):  #
-        c_victim = c_live.index([x, y + 1])
+    elif side == 12 and [x, y + 1] in c_live:
+        index_of_victim = c_live.index([x, y + 1])
         flag = True
-    elif side == 5 and not ([x + 1, y - 1] in c_live):
-        c_victim = c_live.index([x + 1, y - 1])
+    elif side == 13 and [x + 1, y - 1] in c_live:
+        index_of_victim = c_live.index([x + 1, y - 1])
         flag = True
-    elif side == 6 and not ([x, y + 1] in c_live):  #
-        c_victim = c_live.index([x, y + 1])
+    elif side == 14 and [x + 1, y + 1] in c_live:
+        index_of_victim = c_live.index([x + 1, y + 1])
         flag = True
-    elif side == 7 and not ([x + 1, y] in c_live):
-        c_victim = c_live.index([x + 1, y])
+    elif side == 15 and [x + 1, y] in c_live:
+        index_of_victim = c_live.index([x + 1, y])
         flag = True
 
     if flag:
-        live[n][64] += live[c_victim][64]  # забирает жизнь жертвы
-        c_live.pop(c_victim)  # удаляем жертву из координат
-        live.pop(n)
+        live[index_of_bot][64] += live[index_of_victim][64]  # забирает жизнь жертвы
+        c_live.pop(index_of_victim)  # удаляем жертву из координат
+        live.pop(index_of_bot)
+
+# def watch(side, n):
+#     ans = 1                         # 1 - empty, 2 - enemy, 3 - food, 4 - poison
+#     if side == 16:
+#         if [x - 1, y - 1] in c_live:
+#             ans = 2
+#         elif
+#     elif side == 17 and [x - 1, y + 1] in c_live:
+#         index_of_victim = c_live.index([x - 1, y + 1])
+#         flag = True
+#     elif side == 18 and [x - 1, y] in c_live:
+#         index_of_victim = c_live.index([x - 1, y])
+#         flag = True
+#     elif side == 19 and [x, y - 1] in c_live:
+#         index_of_victim = c_live.index([x, y - 1])
+#         flag = True
+#     elif side == 20 and [x, y + 1] in c_live:
+#         index_of_victim = c_live.index([x, y + 1])
+#         flag = True
+#     elif side == 21 and [x + 1, y - 1] in c_live:
+#         index_of_victim = c_live.index([x + 1, y - 1])
+#         flag = True
+#     elif side == 22 and [x + 1, y + 1] in c_live:
+#         index_of_victim = c_live.index([x + 1, y + 1])
+#         flag = True
+#     elif side == 23 and [x + 1, y] in c_live:
+#         index_of_victim = c_live.index([x + 1, y])
+#         flag = True
+
 
 def step():
     n_step = n_live
@@ -121,15 +170,20 @@ def step():
                 n_step.pop(k)
             elif 8<=live[k][c[k]]<=15:  #есть
                 eat(live[k][c[k]], k)
+                rdy+=1
+                live[k][64]-=1
+                n_step.pop(k)
+            # elif 16<=live[k][c[k]]<=23:
+            #     if watch(live[k][c[k]], k):
 
 
-
-def start_color(): #test
+def start_color():                                      #test
     for i in range(size):
         cells[i][0].configure(background='black')
         cells[i][size-1].configure(background='black')
         cells[0][i].configure(background='black')
         cells[size-1][i].configure(background='black')
+
 
 window = Tk()
 window.title("Симулирование эволюции")
@@ -141,6 +195,7 @@ indent = 50
 crt_cell()
 start_color()
 crt_live(int(input('Введите количество организмов:')))
+create_food(10)
 
 while True: #test move
     for i in range(8):
